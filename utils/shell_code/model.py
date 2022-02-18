1279c3c1b611833045073665feb8b67a5a127cbe
roym <roymatza@gmail.com>
2022-02-18 16:53:10 +0200

    changed forward pass function

--- utils/shell_code/model.py
+++ utils/shell_code/model.py
@@ -16,10 +16,13 @@ class MT_RNN_SlowFast(nn.Module):
         self.frequencies = freq
         self.num_freqs = len(freq)
         self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.fusion = True
         self.dropout = torch.nn.Dropout(dropout)
         if rnn_type == "GRU":
             for freq in self.frequencies:
                 GRU_list = []
+                #Seperation of layers
                 dims = [input_dim // 2 if bidirectional else input_dim] + [hidden_dim for layer_i in range(num_layers)]
                 for in_dim, out_dim in zip(dims[:-1], dims[1:]):
                     # print((in_dim * 2 if bidirectional else in_dim, out_dim))
@@ -38,30 +41,70 @@ class MT_RNN_SlowFast(nn.Module):
                                     for s in range(len(num_classes_list))])
 
     def forward(self, rnn_inpus, lengths):
-        outputs=[]
-        freq_outputs = []
         rnn_inpus = rnn_inpus.permute(0, 2, 1)
         rnn_inpus = self.dropout(rnn_inpus)
+        outputs =[]
 
-        #Define different RNNs based on the sampling frequencies
-        for i in range(self.num_freqs):
-            lengths_after_sampling = torch.ceil(lengths / self.frequencies[i]).to(device=lengths.device)
-            sampled_input = rnn_inpus[:, ::self.frequencies[i], :].to(device=rnn_inpus.device)
-            sampled_packed_input = pack_padded_sequence(sampled_input, lengths=lengths_after_sampling, batch_first=True,
-                                                        enforce_sorted=False)
-            temp_output = sampled_packed_input
-            for j, layer_j in enumerate(self.rnns[i]):
-                temp_output, _ = layer_j(temp_output)
-            unpacked_output, _ = torch.nn.utils.rnn.pad_packed_sequence(temp_output, padding_value=-1, batch_first=True)
-            upsample = nn.Upsample(scale_factor=self.frequencies[i])
-            freq_output = upsample(unpacked_output.permute(0, 2, 1))
-            freq_outputs.append(freq_output[:, :, :rnn_inpus.shape[1]])
+        layer_outputs = []
+        #forward pass by layers
+        for layer in range(self.num_layers):
+            #apply lateral connections between paths
+            for i, rnn in enumerate(self.rnns):
+                curr_layer = rnn[layer]
+                if layer==0:
+                    #define different RNNs based on the sampling frequencies
+                    sampled_packed_input = self.create_packed_sampled_input(rnn_inpus, self.frequencies[i], lengths)
+                    layer_output, _ = curr_layer(sampled_packed_input)
+                    layer_outputs.append(layer_output)
+                    continue               
+                
+                layer_output, _ = curr_layer(layer_outputs[i])
+                layer_outputs[i] = layer_output
+                #if self.fusion and i>0:
+                        #layer_outputs.append(self.fuse_outputs(layer_outputs[i], layer_outputs[i-1]))
+        
+        #upsampling the processed sequences
+        for l_i, l_out  in enumerate(layer_outputs):
+            layer_outputs[l_i] = self.create_unpacked_upsampled_output(l_out, self.frequencies[l_i], seq_length=rnn_inpus.shape[1])
 
-        unpacked_rnn_out = torch.cat(freq_outputs, dim=1)
+        #concatenating final result
+        unpacked_rnn_out = torch.cat(layer_outputs, dim=1)
+        
         unpacked_rnn_out = self.dropout(unpacked_rnn_out)
         for output_head in self.output_heads:
             outputs.append(output_head(unpacked_rnn_out.permute(0, 2, 1)).permute(0, 2, 1))
         return outputs
+        
+    def create_packed_sampled_input(self, input, sample_ratio, seq_lengths):
+        '''creating downsampled input packed for RNN'''
+        lengths_after_sampling = torch.ceil(seq_lengths / sample_ratio).to(device=seq_lengths.device)
+        sampled_input = input[:, ::sample_ratio, :].to(device=input.device)
+        
+        return pack_padded_sequence(sampled_input, lengths=lengths_after_sampling, batch_first=True,
+                                                    enforce_sorted=False)
+    
+    def create_unpacked_upsampled_output(self, output, upsample_ratio, seq_length):
+        '''creating the output in an unpacked manner, upsampled to the original frequency'''
+        unpacked_output, _ = torch.nn.utils.rnn.pad_packed_sequence(output, padding_value=-1, batch_first=True)
+        upsample = nn.Upsample(scale_factor=upsample_ratio)
+        output_upsampled = upsample(unpacked_output.permute(0, 2, 1))
+        return output_upsampled[:, :, :seq_length]
+         
+        
+    def fuse_outputs(self, out_small, out_large, sample_ratio, method='sample', op='cat'):
+        '''Applying fusion of two different output tensors, by whether summation or concatenation'''
+        '''Assuming input of shape NxSxD, where N is batchsize, S is sequence length and D is the hidden dimension'''
+
+        if method=='time2channel':
+            out_modified = out_large.view(out_large.shape[0],out_small.shape[1],-1)
+        if method=='sample':
+            out_modified = out_large[:,::sample_ratio,:]
+
+        if op=='cat':
+            return torch.cat(out_modified,out_small,dim=1)
+        if op=='sum':
+            return out_modified+out_small
+
 
 
 class MT_RNN_SlowFast2(nn.Module):
@@ -117,6 +160,9 @@ class MT_RNN_SlowFast2(nn.Module):
         return outputs
 
         #unpacked_rnn_out = torch.cat(unpacked_rnn_out1, unpacked_rnn_out2)
+
+
+
 
 class MT_RNN_dp(nn.Module):
     def __init__(self, rnn_type, input_dim, hidden_dim, num_classes_list, bidirectional, dropout,num_layers=2):
